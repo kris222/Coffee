e0a36920253b720d73b422105d4fce4e5cc65683
Krzysztof Skwarek <krzysztof.skwarek@cads.co.uk>
2022-05-19 16:58:24 +0100

    added quit command

--- WiredBrainCoiffe.ShopInfoTool/Program.cs
+++ WiredBrainCoiffe.ShopInfoTool/Program.cs
@@ -8,18 +8,23 @@ namespace WiredBrainCoiffe.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool");
-            Console.WriteLine("Write 'help' to list available coffee shop commands");
+            Console.WriteLine("Write 'help' to list available coffee shop commands+" +
+                              "write 'quit DateTimeOffset exit application");
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
             while (true)
             {
                 var line = Console.ReadLine();
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
                 if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine(">Available Coffee shop commands:");
                     foreach (var coffeshop in coffeeShops)
                     {
-                            Console.WriteLine($">" + coffeshop.Location);
+                        Console.WriteLine($">" + coffeshop.Location);
                     }
                 }
             }
