3aff4f752c21543a41598a244e64795973271463
Krzysztof Skwarek <krzysztof.skwarek@cads.co.uk>
2022-05-19 16:36:52 +0100

    Implement help command

--- WiredBrainCoiffe.ShopInfoTool/Program.cs
+++ WiredBrainCoiffe.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoiffe.ShopInfoTool
 {
@@ -7,6 +8,21 @@ namespace WiredBrainCoiffe.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool");
+            Console.WriteLine("Write 'help' to list available commands");
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(">Available Coffee shop commands:");
+                    foreach (var coffeshop in coffeeShops)
+                    {
+                            Console.WriteLine($">" + coffeshop.Location);
+                    }
+                }
+            }
         }
     }
 }
